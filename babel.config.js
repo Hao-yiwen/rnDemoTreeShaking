b252dc67f96ccf453d2c25ef30566ff284e6023b
Hao-yiwen <1552975589@qq.com>
2024-10-11 03:16:08 +0800

    feat: 初始化treeshaking工程

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      {
+        disableImportExportTransform: true, // 这里作为 metro-react-native-babel-preset 的配置选项
+      },
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
@@ -6,6 +7,22 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: true,
+      },
+    }),
+    unstable_treeShaking: true,
+  },
+  watchFolders: [
+    path.resolve(__dirname, './node_modules'),
+    // Include necessary file paths for `yarn link`ed modules
+    path.resolve(__dirname, '/Users/yw.hao/Documents/rn/metro/packages'),
+    path.resolve(__dirname, '/Users/yw.hao/Documents/rn/metro/node_modules'),
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
